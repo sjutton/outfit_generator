7f52c9d731d8b4543663dbefbad6950b8d79e985
Sofia Säterli <sofia.saterli@gmail.com>
2024-10-20 16:26:12 +0200

    Level 1 klar. (Sammarbete irl)

--- level1.py
+++ level1.py
@@ -1,27 +1,99 @@
 import random
 
+def cleanup(tröjor, byxor, skor, vattentätQuiz, säsongQuiz, listaTröjor, listaByxor, listaSkor):
+    
+    for item in listaTröjor:
+        placeholder = tröjor.get(item)
+        if placeholder.get("vattentät") != vattentätQuiz:
+            listaTröjor.remove(item)
+        elif säsongQuiz == "all":
+            continue
+        elif placeholder.get("säsong") != säsongQuiz:
+            listaTröjor.remove(item)
+
+    for item in listaByxor:
+        placeholder = byxor.get(item)
+        if placeholder.get("vattentät") != vattentätQuiz:
+            listaByxor.remove(item)
+        elif säsongQuiz == "all":
+            continue
+        elif placeholder.get("säsong") != säsongQuiz:
+            listaByxor.remove(item)
+
+    for item in listaSkor:
+        placeholder = skor.get(item)
+        if placeholder.get("vattentät") != vattentätQuiz:
+            listaSkor.remove(item)
+        elif säsongQuiz == "all":
+            continue
+        elif placeholder.get("säsong") != säsongQuiz:
+            listaSkor.remove(item)
+    print(listaTröjor, listaByxor, listaSkor)
+
+
+
+def quiz():
+    svar = input("Ska outfiten vara vattentät? ja/nej")
+    if svar == "ja":
+        vattentät = True
+    elif svar == "nej":
+        vattentät = False
+    
+    svar = input("Vilken säsong ska outfiten vara anpassad för? (s)ommar/(h)öst/(v)år")
+    if svar == "s":
+        säsong = "sommar"
+    elif svar == "h":
+        säsong = "höst"
+    elif svar == "v":
+        säsong = "vår"
+    elif svar == "a":
+        säsong = "all"
+    
+    return vattentät, säsong
+
 def outfitGenerator():
-    tröjor = ["tshirt", "hoodie"]
-    byxor = ["byxor","finbyxor","shorts"]
-    skor = ["stövlar","pumps","sneakers"]
+    tröjor = {
+        "tshirt":{"vattentät":False, "säsong":"sommar"},
+        "hoodie":{"vattentät":True, "säsong":"höst"}
+     }
+    byxor = {
+        "byxor":{"vattentät":False, "säsong":"höst"},
+        "finbyxor": {"vattentät":False, "säsong":"sommar"},
+        "shorts":{"vattentät":False, "säsong":"sommar"},
+        "galonisar":{"vattentät":True, "säsong":"sommar"}
+    }
+    skor = {
+        "stövlar":{"vattentät":True, "säsong":"höst"},
+        "pumps":{"vattentät":False, "säsong":"sommar"},
+        "sneakers":{"vattentät":False, "säsong":"vår"},
+        "barfota":{"vattentät":False, "säsong":"vår"}
+    }
+
+    listaTröjor = ["tshirt", "hoodie"]
+    listaByxor = ["byxor","finbyxor","shorts","galonisar"]
+    listaSkor = ["stövlar","pumps","sneakers"]    
+
+    vattentätQuiz, säsongQuiz = quiz()
+
+    cleanup(tröjor, byxor, skor, vattentätQuiz, säsongQuiz, listaTröjor, listaByxor, listaSkor)
 
     if len(tröjor) != 1:
-        index = random.randint(0, len(tröjor)-1)
-        choosen_tröja = tröjor[index]
+        index = random.randint(0, (len(listaTröjor)-1))
+        choosen_tröja = listaTröjor[index]
     else:
-        choosen_tröja = tröjor[0]
+        choosen_tröja = listaTröjor[0]
 
     if len(byxor) != 1:
-        index = random.randint(0, len(byxor)-1)
-        choosen_byxa = byxor[index]
+        index = random.randint(0, len(listaByxor)-1)
+        choosen_byxa = listaByxor[index]
     else:
-        choosen_byxa = byxor[0]
+        choosen_byxa = listaByxor[0]
 
     if len(skor) != 1:
-        index = random.randint(0, len(skor)-1)
-        choosen_sko = skor[index]
+        index = random.randint(0, len(listaSkor)-1)
+        choosen_sko = listaSkor[index]
     else:
-        choosen_sko = skor[0]
+        choosen_sko = listaSkor[0]
     
     return choosen_tröja, choosen_byxa, choosen_sko
 
